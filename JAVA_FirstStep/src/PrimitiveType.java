9ec2b63b030011d1074ff5e5f94662be4f609629
jooho-le <wngh0606@naver.com>
2026-04-30 10:11:31 +0900

    기본형 2

--- JAVA_FirstStep/src/PrimitiveType.java
+++ JAVA_FirstStep/src/PrimitiveType.java
@@ -7,6 +7,15 @@ public class PrimitiveType {
         // 3. int(= 4bytes)
         // 4. long(= 8bytes)
 
-        byte a = 128; // 범위를 넘어서 오류 발ㅅ애
+        // byte a = 128; // 범위를 넘어서 오류 발생
+
+        // 실수형 구분
+        // 1. double(= 8bytes) - 15자리 , 주로 많이 사용
+        // 2. float(= 4bytes) - 7자리
+
+        double a = 1.23;
+        float b = 9.45f;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
