394e1ad06d613742b7aa4dd71689210b30159c40
jooho-le <wngh0606@naver.com>
2026-04-30 11:00:48 +0900

    String 3

--- JAVA_FirstStep/src/StringExample.java
+++ JAVA_FirstStep/src/StringExample.java
@@ -5,30 +5,44 @@ public class StringExample {
         // 객체 : 힙 메모리 영역
         // 변수 : 힙 메모리 영역의 주소
 
-        String str = "안녕하세요"; // 문자열 리터럴
-        String str_2 = new String("안녕하세요"); // 생성자
-        String str_3 = "안녕하세요";
+//        String str = "안녕하세요"; // 문자열 리터럴
+//        String str_2 = new String("안녕하세요"); // 생성자
+//        String str_3 = "안녕하세요";
+//
+//        if (str == str_2) {
+//            System.out.println("str == str_2");
+//        }
+//        if (str == str_3) {
+//            System.out.println("str == str_3");
+//        }
+//        if (str_2 == str_3) {
+//            System.out.println("str2 == str_3");
+//        }
+//
+//        // 내용 비교
+//        if (str.equals(str_2)) {
+//            System.out.println("str.equals(str_2)");
+//        }
+//        if (str.equals(str_3)) {
+//            System.out.println("str.equals(str_3)");
+//        }
+//        if (str_2.equals(str_3)) {
+//            System.out.println("str_2.equals(str_3)");
+//        }
 
-        if (str == str_2) {
-            System.out.println("str == str_2");
-        }
-        if (str == str_3) {
-            System.out.println("str == str_3");
-        }
-        if (str_2 == str_3) {
-            System.out.println("str2 == str_3");
-        }
+        // 문자열 병합
+        // 1. '+' 연산자
+        String str_1 = "hello";
+        String str_2 = "world";
+        System.out.println(str_1 + " " + str_2);
 
-        // 내용 비교
-        if (str.equals(str_2)) {
-            System.out.println("str.equals(str_2)");
-        }
-        if (str.equals(str_3)) {
-            System.out.println("str.equals(str_3)");
-        }
-        if (str_2.equals(str_3)) {
-            System.out.println("str_2.equals(str_3)");
-        }
+        // 2. StringBuilder
+        // 속도, 메모리 측면에서 유리한 방법
+        StringBuilder strBdr_1 = new StringBuilder("hello");
+        strBdr_1.append("  ");
+        strBdr_1.append("world");
+        String str_new = strBdr_1.toString();
+        System.out.println(str_new);
 
     }
 }
