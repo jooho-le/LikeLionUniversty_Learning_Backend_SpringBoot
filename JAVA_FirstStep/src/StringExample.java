8885904b13a34f0823eb6755b9d8a5ee12127ebd
jooho-le <wngh0606@naver.com>
2026-04-30 11:12:02 +0900

    String 5

--- JAVA_FirstStep/src/StringExample.java
+++ JAVA_FirstStep/src/StringExample.java
@@ -50,20 +50,32 @@ public class StringExample {
 //        System.out.println(str_name);
 
         // 문자열 대소문자 변환
-        String str_1 = "abc";
-        String str_2 = "ABC";
-        str_1 = str_1.toUpperCase();
-        str_2 = str_2.toLowerCase();
-        System.out.println(str_1);
-        System.out.println(str_2);
+//        String str_1 = "abc";
+//        String str_2 = "ABC";
+//        str_1 = str_1.toUpperCase();
+//        str_2 = str_2.toLowerCase();
+//        System.out.println(str_1);
+//        System.out.println(str_2);
+//
+//        if (str_1.equals(str_2)) {
+//            System.out.println(str_1.equals(str_2));
+//        }
+//        // 대소문자 무시하고 값을 비교
+//        if (str_1.equalsIgnoreCase(str_2)) {
+//            System.out.println("str_1.equals(str_2)");
+//        }
 
-        if (str_1.equals(str_2)) {
-            System.out.println(str_1.equals(str_2));
-        }
-        // 대소문자 무시하고 값을 비교
-        if (str_1.equalsIgnoreCase(str_2)) {
-            System.out.println("str_1.equals(str_2)");
-        }
+        // 공백제거
+        // 1. 양쪽 끝 공백
+        String str_1 = "       hello   ";
+        str_1 = str_1.trim();
+        System.out.println(str_1);
+
+        // 2. 문자열 중간 공백
+        String str_2 = "    hel   lo   ";
+        str_2 = str_2.trim();
+        str_2 = str_2.replace(" ", "");
+        System.out.println(str_2);
 
     }
 }
