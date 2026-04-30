d12b80ee246f68cea6ed26ad8f3c6bd76a1ed06f
jooho-le <wngh0606@naver.com>
2026-04-30 10:09:07 +0900

    기본형 1

---
+++ JAVA_FirstStep/src/PrimitiveType.java
@@ -0,0 +1,12 @@
+public class PrimitiveType {
+    public static void main(String[] args) {
+
+        // 정수형 구분
+        // 1. byte(= 8bits)
+        // 2. short(= 2bytes)
+        // 3. int(= 4bytes)
+        // 4. long(= 8bytes)
+
+        byte a = 128; // 범위를 넘어서 오류 발ㅅ애
+    }
+}
