119454aeb1373b80cbdcd424c9f95f863e618f3f
jooho-le <wngh0606@naver.com>
2026-04-30 11:43:12 +0900

    연산자 1

---
+++ JAVA_FirstStep/JAVA Beginner/src/Operators.java
@@ -0,0 +1,33 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        // 산술 연산자
+//        int a = 10;
+//        int b = 20;
+//        String c = "abc";
+//        double d = 10;
+//        double e = 20;
+//
+//        System.out.println(d / e);
+//
+//        System.out.println(a + b);
+//        System.out.println(a - b);
+//        System.out.println(a * b);
+//        System.out.println(a / b);
+//        System.out.println(a % b);
+//
+//        System.out.println(a + c);
+
+        // 비교 연산자
+        int a = 5;
+        int b = 8;
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a >= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+    }
+}
