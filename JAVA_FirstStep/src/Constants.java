0f0931660faf1598fc114953280e911cfefa7c1f
jooho-le <wngh0606@naver.com>
2026-04-30 10:24:31 +0900

    상수

---
+++ JAVA_FirstStep/src/Constants.java
@@ -0,0 +1,14 @@
+public class Constants {
+    public static void main(String[] args) {
+
+        // 상수
+        // 한번 값이 할당 후 재할당이 불가
+        // 전체를 대문자로, 단어간 구분은 _로 구분
+
+        final double PI = 3.14;
+        System.out.println(PI);
+        //PI = 3.141592; // 재할당 불가
+
+
+    }
+}
