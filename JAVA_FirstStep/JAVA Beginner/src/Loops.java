9b90520b717c3f384eb8901ec472e686707df856
jooho-le <wngh0606@naver.com>
2026-05-02 02:37:37 +0900

    반복문 2

--- JAVA_FirstStep/JAVA Beginner/src/Loops.java
+++ JAVA_FirstStep/JAVA Beginner/src/Loops.java
@@ -2,9 +2,25 @@ public class Loops {
     public static void main(String[] args) {
 
         // 1. for
-        // 초기화된 변수, 조건식, 증감식
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
+//        // 초기화된 변수, 조건식, 증감식
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(i);
+//        }
+
+        // 2. while
+        int b = 10;
+
+//        while (b > 0) {
+//            System.out.println(b);
+//            b--;
+//        }
+
+        // 3. do while
+        // 무조건 한번 실행
+        do {
+            System.out.println(b);
+            b--;
+        } while (b > 0);
+
     }
 }
