9720852000dfc7d618b86417f8049916e48019ac
jooho-le <wngh0606@naver.com>
2026-05-02 02:40:15 +0900

    배열 1

---
+++ JAVA_FirstStep/JAVA Beginner/src/ArrayExample.java
@@ -0,0 +1,11 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+
+        // 배열
+        int[] price = {10000, 9000, 40000, 7000};
+        String[] mbti = {"entj", "intj"};
+
+        System.out.println(price[0]);
+        System.out.println(mbti[0]);
+    }
+}
