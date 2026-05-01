52f4451578eb2e66e30c686619c872fcbe350366
jooho-le <wngh0606@naver.com>
2026-05-02 02:35:15 +0900

    반복문 1

---
+++ JAVA_FirstStep/JAVA Beginner/src/Loops.java
@@ -0,0 +1,10 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        // 1. for
+        // 초기화된 변수, 조건식, 증감식
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
