21cfaa68a2c9ce5bdd5e423227eb81040b3d263a
jooho-le <wngh0606@naver.com>
2026-05-02 02:33:03 +0900

    조건문 2

--- JAVA_FirstStep/JAVA Beginner/src/Conditional.java
+++ JAVA_FirstStep/JAVA Beginner/src/Conditional.java
@@ -1,7 +1,7 @@
 public class Conditional {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 100;
+//        int a = 1;
+//        int b = 100;
 
 //        if (a != b) {
 //            System.out.println("a != b");
@@ -13,15 +13,32 @@ public class Conditional {
 //            System.out.println("else block");
 //        }
 
-        if (a == b) {
-            System.out.println("a==b");
-        } else if (a < b) {
-            System.out.println("a<b");
-        } else {
-            System.out.println("a>b");
-        }
+//        if (a == b) {
+//            System.out.println("a==b");
+//        } else if (a < b) {
+//            System.out.println("a<b");
+//        } else {
+//            System.out.println("a>b");
+//        }
 
         // 다중 조건의 경우 조건을 만족하는 최초의 분기만 실행
+
+        // switch
+        int a = 10;
+
+        switch (a + 1) {
+            case 9:
+                System.out.println("a+1=9");
+                break;
+            case 10:
+                System.out.println("a+1=10");
+                break;
+            case 11:
+                System.out.println("a+1=11");
+                break;
+            default:
+                System.out.println("default");
+        }
 
     }
 }
